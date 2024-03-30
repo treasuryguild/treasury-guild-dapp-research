edef45725e64df627803b9bcac2a2d5c7aa864d9
André Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2024-03-30 06:31:22 +0200

    Remove unused Netlify functions and update .gitignore

--- functions/processTransaction.js
+++ functions/processTransaction.js
@@ -1,8 +1,18 @@
-// .netlify/functions/processTransaction.js
-import updateTransactionTables from './updateTransactionTables';
+//  ../functions/processTransaction.js
+import updateTransactionTables from '../utils/updateTransactionTables';
 import { supabaseAdmin } from '../lib/supabaseClient';
 
 export async function handler(event, context) {
+  const contentType = event.headers['content-type'];
+
+  // Check if the content type is JSON
+  if (contentType !== 'application/json') {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Invalid content type. Expected application/json.' }),
+    };
+  }
+
   const { record } = JSON.parse(event.body);
 
   try {
@@ -11,13 +21,16 @@ export async function handler(event, context) {
       .from('pending_transactions')
       .update({ processed: true })
       .eq('id', record.id);
+
     console.log('Transaction processed successfully:', record.id);
+
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Transaction processed successfully' }),
     };
   } catch (error) {
     console.error('Error processing transaction:', error);
+
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Error processing transaction' }),

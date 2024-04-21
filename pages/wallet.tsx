f2b852ab8ca40f525d5f50452beabfad819210aa
André Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2024-04-21 04:53:21 +0200

    Add blockchain selection functionality to WalletPage component

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -1,10 +1,13 @@
 // This could be any page inside the pages directory, for example, pages/about.js
 import TxLayout from '../layouts/TxLayout';
 import WalletConnector from '../components/WalletConnector';
+import { useState } from 'react';
 
 export default function WalletPage() {
+  const [blockchain, setBlockchain] = useState('Polkadot');
+
   return (
-    <TxLayout>
+    <TxLayout blockchain={blockchain}>
       <h1>Wallets</h1>
       <p>This is the wallets page, all balances can be viewed here</p>
       <WalletConnector />

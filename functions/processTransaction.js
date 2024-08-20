92d1323a3b78744276e365a6fb2b0ef995e11f98
André Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2024-08-20 07:49:41 +0200

    feat: Update processTransaction.js to use NEXT_PUBLIC_SUPABASE_URL

--- functions/processTransaction.js
+++ functions/processTransaction.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 import updateTransactionTables from '../utils/updateTransactionTables';
 
-const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey, {
